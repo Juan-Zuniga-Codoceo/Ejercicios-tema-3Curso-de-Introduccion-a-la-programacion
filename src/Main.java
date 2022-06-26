d1d1356d39781e2fd171be340382576298540cdc
Juan-Zuniga-Codoceo <zcja.89@gmail.com>
2022-06-25 21:31:58 -0400

    1

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args){
+        //Llamar a la función en el main y darle valores
+        suma(10,20,30);
+        //Crear un objeto miCoche en el main y añadirle una puerta.
+        Coche miCoche = new Coche();
+        System.out.println(miCoche.puertas);
+        miCoche.incrementarPuertas();
+        System.out.println(miCoche.puertas);
+    }
+
+    //Crear una función con tres parámetros que sean números que se suman entre sí.
+    public static int suma(int a, int b, int c){
+        int resultado;
+        resultado = a+b+c;
+
+        System.out.println(resultado); // para mostrar por consola
+
+        return resultado;
+    }
+}
+
+//Crear una clase coche.
+class Coche {
+    //Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+   public int puertas = 0;
+   //Una función que incremente el número de puertas que tiene el coche.
+    public void incrementarPuertas(){
+        this.puertas++;
+    }
+}
